73988945bf930cbddfbe79deef6764c4318c26af
Kel-bees <82900599+Kel-bees@users.noreply.github.com>
2021-07-08 17:51:05 -0500

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -7,12 +7,11 @@ module.exports = {
         if(target) {
 
             let mainRole = message.guild.roles.cache.find(role => role.name === 'Miembro');
-            let muteRole = message.guild.roles.cache.find(role => role.name === 'Muteado');
+            let muteRole = message.guild.roles.cache.find(role => role.name === 'Muted');
 
             let memberTarget = message.guild.members.cache.get(target.id);
             
-            if (!args[1]) {
-            if(message.member.roles.cache.some(r => r.name === "Owner")){    
+            if (!args[1]) {   
                 memberTarget.roles.remove(mainRole.id);
                 memberTarget.roles.add(muteRole.id);
                 message.channel.send(`<@${memberTarget.user.id}> fue muteado!`);
