8f8da6dfdaad861490a3925a36fcc4f7eb96f301
Kel-bees <82900599+Kel-bees@users.noreply.github.com>
2021-07-08 17:50:42 -0500

    Add files via upload

---
+++ commands/mute.js
@@ -0,0 +1,30 @@
+module.exports = {
+    name: 'mute',
+    description: "Mutea!",
+    permissions: ["ADMINISTRATOR"],
+    execute(client, message, cmd, args, Discord) {
+        const target = message.mentions.users.first();
+        if(target) {
+
+            let mainRole = message.guild.roles.cache.find(role => role.name === 'Miembro');
+            let muteRole = message.guild.roles.cache.find(role => role.name === 'Muteado');
+
+            let memberTarget = message.guild.members.cache.get(target.id);
+            
+            if (!args[1]) {
+            if(message.member.roles.cache.some(r => r.name === "Owner")){    
+                memberTarget.roles.remove(mainRole.id);
+                memberTarget.roles.add(muteRole.id);
+                message.channel.send(`<@${memberTarget.user.id}> fue muteado!`);
+                return
+            }
+            memberTarget.roles.remove(mainRole.id);
+            memberTarget.roles.add(muteRole.id);
+            message.channel.send(`<@${memberTarget.user.id}> fue muteado`);
+
+        }else{ 
+            message.channel.send('No encontre ese miembro!');
+        }
+    }
+}
+}
