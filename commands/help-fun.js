b2337d55ea9a84e6ef8512170a952b59f3563d64
Kel-bees <82900599+Kel-bees@users.noreply.github.com>
2021-07-08 17:45:53 -0500

    Update help-fun.js

--- commands/help-fun.js
+++ commands/help-fun.js
@@ -4,17 +4,17 @@ module.exports = {
     permissions: [],
     execute(client, message, cmd, args, Discord) {
         const newEmbed = new Discord.MessageEmbed()
-        .setColor('#FDEAFF')
+        .setColor('#F9F99A')
         .setTitle('Comandos de diversion')
         .setDescription('Diversion!')
         .addFields(
-            {name: 'GT!8ball', value: 'Pregunta lo que sea,te respondere!'},
-            {name: 'GT!PPT', value: 'Piedra papel o tijeras!'},
-            {name: 'GT!tictactoe', value: 'Juego de tictactoe.'},
-            {name: 'GT!meme', value: ' memes!.'},
+            {name: '%8ball', value: 'Pregunta lo que sea,te respondere!'},
+            {name: '%PPT', value: 'Piedra papel o tijeras!'},
+            {name: '%tictactoe', value: 'Juego de tictactoe.'},
+            {name: '%meme', value: ' memes!.'},
 
         )
-        .setImage('https://cdn.discordapp.com/avatars/854477639514587157/88427f9bee8b1b01234a05e7224fb9dd.webp?size=256')
+        .setImage('https://cdn.discordapp.com/avatars/853511511472734218/62383ce3b40480725f4f5a28ecab7483.webp?size=256')
         .setFooter('Bot creado por Bumblebee Kev#4596');
         
         message.channel.send(newEmbed)
