90cbf52b7789f7b0a42e74d0ff8ae0b31dc25cd0
Kel-bees <82900599+Kel-bees@users.noreply.github.com>
2021-07-08 17:50:11 -0500

    Update unmute.js

--- commands/unmute.js
+++ commands/unmute.js
@@ -6,14 +6,14 @@ module.exports = {
         const target = message.mentions.users.first();
         if(target){
 
-            let mainRole = message.guild.roles.cache.find(role => role.name === 'member')
+            let mainRole = message.guild.roles.cache.find(role => role.name === 'Miembro')
             let muteRole = message.guild.roles.cache.find(role => role.name === 'Muted');
  
             let memberTarget= message.guild.members.cache.get(target.id);
  
             memberTarget.roles.remove(muteRole.id);
             memberTarget.roles.add(mainRole.id);
-            message.channel.send(`<@${memberTarget.user.id}> was unmuted!`);
+            message.channel.send(`<@${memberTarget.user.id}> fue desmuteado!`);
         } else{
             message.channel.send('I couldnt find that member!');
         }
