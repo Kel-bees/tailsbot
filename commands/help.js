397403be121a60ac6e1852c065229694fead295b
Kel-bees <82900599+Kel-bees@users.noreply.github.com>
2021-07-08 17:53:03 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -4,14 +4,15 @@ module.exports = {
     permissions: [],
     execute(client, message, cmd, args, Discord) {
         const newEmbed = new Discord.MessageEmbed()
-        .setColor('#FDEAFF')
+        .setColor('#F9F99A')
         .setTitle('Mis comandos')
         .setDescription('Todos mis comandos!')
         .addFields(
-            {name: 'GT!help-fun', value: 'Comandos de diversion!'},
-            {name: 'GT!help-mod', value: 'Comandos de moderacion!'},
+            {name: '%help-fun', value: 'Comandos de diversion!'},
+            {name: '%help-mod', value: 'Comandos de moderacion!'},
+            {name: '%help-mod', value: 'Comandos Random!'}
         )
-        .setImage('https://cdn.discordapp.com/avatars/854477639514587157/88427f9bee8b1b01234a05e7224fb9dd.webp?size=256')
+        .setImage('https://cdn.discordapp.com/avatars/853511511472734218/62383ce3b40480725f4f5a28ecab7483.webp?size=256')
         .setFooter('Bot creado por Bumblebee Kev#4596');
         
         message.channel.send(newEmbed)
