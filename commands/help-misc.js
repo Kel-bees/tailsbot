0043cd5aaa879c30ce82a2f1065118803841b9e1
Kel-bees <82900599+Kel-bees@users.noreply.github.com>
2021-07-08 17:52:01 -0500

    Update help-misc.js

--- commands/help-misc.js
+++ commands/help-misc.js
@@ -4,14 +4,14 @@ module.exports = {
     permissions: [],
     execute(client, message, cmd, args, Discord) {
         const newEmbed = new Discord.MessageEmbed()
-        .setColor('#85ca87')
+        .setColor('#F9F99A')
         .setTitle('Comandos random')
         .setDescription('Random!')
         .addFields(
-            {name: '*poll', value: 'Crea una encuesta!'},
-            {name: '*suggest', value: 'Haz una sugerencia!'},
+            {name: '%poll', value: 'Crea una encuesta!'},
+            {name: '%suggest', value: 'Haz una sugerencia!'},
         )
-        .setImage('https://cdn.discordapp.com/avatars/861326507221057598/54b7ec8e2b1e6d1cd2a06fb23d7105c6.webp?size=256')
+        .setImage('https://cdn.discordapp.com/avatars/853511511472734218/62383ce3b40480725f4f5a28ecab7483.webp?size=256')
         .setFooter('Bot creado por Bumblebee Kev#4596');
         
         message.channel.send(newEmbed)
